3ee3c3cd8bcf4172a1e4106a2c416d5faa3a5bcd
JP-sDEV <jon.pablo80@gmail.com>
2025-05-13 15:14:26 -0400

    fix: temp fix for azure packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [new URL("https://dz2cdn1.dzone.com/**")],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ensure the externals array exists
+      config.externals = config.externals || [];
+
+      // Add the specific modules you want to externalize
+      config.externals.push("@azure/functions-core");
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
