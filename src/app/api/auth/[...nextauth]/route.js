77a86ab5bba23a4bee8e44389e8cfdcd5fb38b74
JP-sDEV <jon.pablo80@gmail.com>
2025-05-13 15:39:38 -0400

    fix: add nextauth secret

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
 export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
