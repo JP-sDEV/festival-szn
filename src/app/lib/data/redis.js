6e031d1f622c6a47c2418d4af4302daa59074593
JP-sDEV <jon.pablo80@gmail.com>
2025-05-13 15:26:47 -0400

    config: change to azure

--- src/app/lib/data/redis.js
+++ src/app/lib/data/redis.js
@@ -1,15 +1,15 @@
 import Redis from "ioredis";
 
-// const redis = new Redis({
-//   host: process.env.REDIS_HOST, // e.g., your-redis.redis.cache.windows.net
-//   port: process.env.REDIS_PORT, // Azure uses SSL on port 6380
-//   password: process.env.REDIS_KEY, // Access key from Azure
-//   tls: {}, // Required for Azure Redis
-// });
-
 const redis = new Redis({
-  host: "localhost",
-  port: 6379,
+  host: process.env.REDIS_HOST, // e.g., your-redis.redis.cache.windows.net
+  port: process.env.REDIS_PORT, // Azure uses SSL on port 6380
+  password: process.env.REDIS_KEY, // Access key from Azure
+  tls: {}, // Required for Azure Redis
 });
+
+// const redis = new Redis({
+//   host: "localhost",
+//   port: 6379,
+// });
 
 export default redis;
